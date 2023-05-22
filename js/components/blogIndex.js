709709e0eea3c85e08792b172c53c8c92adcb0f5
Kim <kim@kimrune.dev>
2023-05-22 13:45:35 +0200

    working on post filters

--- js/components/blogIndex.js
+++ js/components/blogIndex.js
@@ -26,6 +26,46 @@ export default function blogIndex(posts) {
         showPosts(data, position, counter);
       });
     });
+
+  const filter_close = document.querySelector(".filter-menu__close");
+  const width = window.innerWidth;
+  if (width < 768) {
+    const filter_menu_filters = document.querySelector(".filter-menu .filters");
+    filter_menu_filters.classList.remove("open");
+  }
+  window.addEventListener("resize", handleResize);
+
+  filter_close.addEventListener("click", () => {
+    const filter_close_icon = document.querySelector(
+      ".filter-menu__close-icon"
+    );
+    const filter_open_icon = document.querySelector(".filter-menu__open-icon");
+    filter_close_icon.classList.toggle("hide");
+    filter_open_icon.classList.toggle("hide");
+
+    const filter_menu_filters = document.querySelector(".filter-menu .filters");
+    filter_menu_filters.classList.toggle("open");
+  });
+}
+
+function handleResize() {
+  const filter_close_icon = document.querySelector(".filter-menu__close-icon");
+  const filter_open_icon = document.querySelector(".filter-menu__open-icon");
+  if (window.innerWidth < 768) {
+    const filter_menu_filters = document.querySelector(".filter-menu .filters");
+    filter_menu_filters.classList.remove("open");
+    if (!filter_close_icon.classList.contains("hide")) {
+      filter_close_icon.classList.add("hide");
+      filter_open_icon.classList.remove("hide");
+    }
+  } else {
+    const filter_menu_filters = document.querySelector(".filter-menu .filters");
+    filter_menu_filters.classList.add("open");
+    if (!filter_open_icon.classList.contains("hide")) {
+      filter_open_icon.classList.add("hide");
+      filter_close_icon.classList.remove("hide");
+    }
+  }
 }
 
 function showPosts(data, position, counter) {
